163210b5659d674d8a04913fd1d6c588a812034c
Diego Alejandro Diaz <90485793+AlejandroDeveloper2@users.noreply.github.com>
2025-11-08 12:23:30 -0500

    cambio

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    data.results.forEach(({ name, url }) => {
+    const pokemons = data.results.map(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
+      return { no, name };
     });
-    return data.results;
+
+    await this.pokemonModel.insertMany(pokemons);
+
+    return 'Seed executed';
   }
 }
